982d2c2c5d728b4d894437e330bf524b5ea60afe
andre-alck <andrealck1@gmail.com>
2023-04-04 23:42:53 -0300

    refactor: implementacao do metodo promover e demitir na propria classe

--- erp/src/main/java/com/me/erp/participante/interno/funcionario/supervisor/Supervisor.java
+++ erp/src/main/java/com/me/erp/participante/interno/funcionario/supervisor/Supervisor.java
@@ -4,7 +4,12 @@ import com.me.erp.participante.interno.funcionario.Funcionario;
 import com.me.erp.participante.interno.funcionario.clt.Clt;
 
 public abstract class Supervisor extends Clt {
-    public abstract boolean promover(Funcionario funcionarioASerPromovido);
+    public boolean promover(Funcionario funcionarioASerPromovido) {
+        return true;
+    }
 
-    public abstract boolean demitirFuncionario(Funcionario funcionarioASerDemitido);
+
+    public boolean demitirFuncionario(Funcionario funcionarioASerDemitido) {
+        return true;
+    }
 }
