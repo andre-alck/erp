23c76e868b9f601bba59a5f07e27701d5f3216db
andre-alck <andrealck1@gmail.com>
2023-04-29 17:27:30 -0300

    refactor: add getters e setters para credenciais

--- erp/src/main/java/com/me/erp/participante/interno/Credenciais.java
+++ erp/src/main/java/com/me/erp/participante/interno/Credenciais.java
@@ -1,6 +1,22 @@
 package com.me.erp.participante.interno;
 
 public class Credenciais {
-    String id;
-    String senha;
+    private String id;
+    private String senha;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
