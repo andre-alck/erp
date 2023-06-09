a82932dd41b36416f5798334a2b3f7073873dcf9
andre-alck <andrealck1@gmail.com>
2023-06-08 22:08:08 -0300

    feat: init controller para EstagiarioDeti; init metodo para conexao entre controller e model para EstagiarioDeTi

--- erp/src/main/java/com/me/erp/participante/interno/funcionario/estagiario/estagiariodeti/EstagiarioDeTi.java
+++ erp/src/main/java/com/me/erp/participante/interno/funcionario/estagiario/estagiariodeti/EstagiarioDeTi.java
@@ -1,11 +1,15 @@
 package com.me.erp.participante.interno.funcionario.estagiario.estagiariodeti;
 
+import com.me.erp.dao.Dao;
+import com.me.erp.participante.interno.funcionario.estagiario.Documentacao;
 import com.me.erp.participante.interno.funcionario.estagiario.Estagiario;
 import com.me.erp.participante.interno.funcionario.ti.atividadestiniveljunior.AtividadesTiNivelJunior;
 
 public class EstagiarioDeTi extends Estagiario {
 
   private AtividadesTiNivelJunior atividadesTiNivelJunior;
+
+  private Dao dao;
 
   public EstagiarioDeTi() {}
 
@@ -19,5 +23,10 @@ public class EstagiarioDeTi extends Estagiario {
 
   public String programar() {
     return this.atividadesTiNivelJunior.programar();
+  }
+
+  public void documentar(Documentacao documentacao) {
+    super.documentar(documentacao);
+    dao.registraNovaTarefa(this.getId(), documentacao.toString());
   }
 }
