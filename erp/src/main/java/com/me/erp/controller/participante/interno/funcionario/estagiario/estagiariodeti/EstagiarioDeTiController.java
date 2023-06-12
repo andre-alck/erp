65090f6e7b3695606834a843b9d0564aa467a513
andre-alck <andrealck1@gmail.com>
2023-06-12 01:03:08 -0300

    feat: add metodo documentar para EstagiarioDeTiController

--- erp/src/main/java/com/me/erp/controller/participante/interno/funcionario/estagiario/estagiariodeti/EstagiarioDeTiController.java
+++ erp/src/main/java/com/me/erp/controller/participante/interno/funcionario/estagiario/estagiariodeti/EstagiarioDeTiController.java
@@ -1,3 +1,18 @@
 package com.me.erp.controller.participante.interno.funcionario.estagiario.estagiariodeti;
 
-public class EstagiarioDeTiController {}
+import com.me.erp.participante.interno.funcionario.estagiario.Documentacao;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("estagiariodeti")
+public class EstagiarioDeTiController {
+
+  @PostMapping("/documentar")
+  public ResponseEntity documentar(@RequestBody Documentacao documentacao) {
+    return ResponseEntity.status(200).build();
+  }
+}
